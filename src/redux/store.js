46f79be4daa146d8aaa70877e73733ffc33b7580
Daniel Richter <drichter3697@gmail.com>
2024-08-15 10:40:28 -0500

    completed page 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,30 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
-// Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+const cart = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TO_ORDER':
+      return [...state, action.payload]
+    case 'REMOVE_FROM_ORDER':
+      return state.filter((pizza) => !action.payload.id)
+  }
   return state;
+}
+
+const total = (state = 0, action) => {
+  switch(action.type) {
+    case 'ADD_TO_ORDER':
+      return state + Number(action.payload.price)
+    case 'REMOVE_FROM_ORDER':
+      return state - Number(action.payload.price)
+  }
+  return state
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    cart,
+    total
   }),
   applyMiddleware(logger),
 );
