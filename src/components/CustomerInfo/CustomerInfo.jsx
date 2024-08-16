f697c97499f5a6cef674b74c6ab11cf1ad7b47fe
Emma Molden <emma.molden007@gmail.com>
2024-08-16 09:50:17 -0500

    header looks goog on every page

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -89,7 +89,7 @@ export default function CustomerInfo() {
           <FormControlLabel
             value="Delivery"
             control={<Radio />}
-            label="Pickup"
+            label="Delivery"
           />
           </RadioGroup>
         <Button onClick={handleCustomerSubmit}>Next</Button>
